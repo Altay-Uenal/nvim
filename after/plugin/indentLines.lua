141e7ce933e9786a6b98e9adc721f61e957b4b0d
Altay Uenal <altayuenal@gmail.com>
2023-10-10 09:46:12 -0600

    updated ibl

--- after/plugin/indentLines.lua
+++ after/plugin/indentLines.lua
@@ -1,6 +1,7 @@
-require("ibl").setup {
-    show_first_indent_level = true,
-    show_trailing_blankline_indent = false,
-    use_treesitter = true,
-    show_current_context = true,
-}
+--require("ibl").setup {
+--    show_first_indent_level = true,
+--    show_trailing_blankline_indent = false,
+--    use_treesitter = true,
+--    show_current_context = true,
+--}
+require("ibl").setup()
