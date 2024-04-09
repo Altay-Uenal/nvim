fee9b75795d15abb87118342fea4c2f66c9800e6
Altay Uenal <altayuenal@gmail.com>
2024-04-08 21:47:00 -0600

    removed old comments

--- lua/packages.lua
+++ lua/packages.lua
@@ -9,8 +9,7 @@ return require('packer').startup(function(use)
   use { "catppuccin/nvim", as = "catppuccin" }
   use { "ellisonleao/gruvbox.nvim" }
   use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-    -- or                            , branch = '0.1.x',
+    'nvim-telescope/telescope.nvim', tag = '0.1.5', -- or                            , branch = '0.1.x',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
 
@@ -45,27 +44,4 @@ return require('packer').startup(function(use)
       {'L3MON4D3/LuaSnip'},
     }
   }
-  --old lsp setup
-  --use {
-  --  'VonHeikemen/lsp-zero.nvim',
-  --  branch = 'v3.x',
-  --  requires = {
-  --      -- LSP Support
-  --      {'neovim/nvim-lspconfig'},             -- Required
-  --      {'williamboman/mason.nvim'},           -- Optional
-  --      {'williamboman/mason-lspconfig.nvim'}, -- Optional
-
-  --      -- Autocompletion
-  --      {'hrsh7th/nvim-cmp'},         -- Required
-  --      {'hrsh7th/cmp-nvim-lsp'},     -- Required
-  --      {'hrsh7th/cmp-buffer'},       -- Optional
-  --      {'hrsh7th/cmp-path'},         -- Optional
-  --      {'saadparwaiz1/cmp_luasnip'}, -- Optional
-  --      {'hrsh7th/cmp-nvim-lua'},     -- Optional
-
-  --      -- Snippets
-  --      {'L3MON4D3/LuaSnip'},             -- Required
-  --      {'rafamadriz/friendly-snippets'}, -- Optional
-  --  }
-  --}
 end)
